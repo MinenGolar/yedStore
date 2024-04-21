454e85d49a193cb45aa3e0430e31a8429624f9a0
GolarMinen <golarminen9@gmail.com>
2024-04-21 17:35:54 +0100

    axios

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,20 +2,20 @@ import User from "../models/userModel.js"
 
 const addUser = async (req,res) =>{
     const {fName, Mail, pNumber,pWord} = req.body;
-    if (!fName || !fName || !Mail || !pNumber || !pWord){
+    if (!fName || !Mail || !pNumber || !pWord){
         return res.status(400).json({message:"Please fill in your details."})
     }
     try {
-        const user = await User.findOne ({email})
+        const user = await User.findOne ({Mail})
         if (user) {
             return res.status(400).json({message:"User already exists."})
         }
 
     User.create({
-        fName: "Minengolar",
-        Mail: "winter@gmail.com",
-        pNumber: "0712345678",
-        pWord: "<PASSWORD>"
+        fName,
+        Mail,
+        pNumber,
+        pWord,
     })
 
     res.send ('i was successfully created')
